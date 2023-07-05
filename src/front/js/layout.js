02782a95917f7135a6108f11702b558a5c68f39e
julitar <126813769+julitar@users.noreply.github.com>
2023-07-05 18:10:14 +0000

    Login

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,6 +12,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/login";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +30,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<Login />} path="/login" />
                         <Route element={<Ong_register />} path="/ong_registration" />
                         <Route element={<User_register />} path="/user_registration" />
                         <Route element={<Single />} path="/single/:theid" />
