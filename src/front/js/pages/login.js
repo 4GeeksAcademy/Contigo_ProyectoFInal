77f26ef7352c582dab03c1f18ccf517737c1cc20
julitar <126813769+julitar@users.noreply.github.com>
2023-07-07 16:04:53 +0000

    Diseño login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/login.css";
 
 
 export const Login = () => {
@@ -10,42 +11,42 @@ export const Login = () => {
     <div className= "container justify-content-center">
         <div className= "row justify-content-center">
 
-            <div className="col-lg-3 col-md-6 h-100 p-5 mx-5 mb-3 bg-light border rounded-3 text-center align-self-center shadow">
+            <div className="jumbotron_1 col-lg-3 col-md-6 h-100 p-5 mx-5 mb-3 bg-light border rounded-3 text-center align-self-center shadow">
                 <form>
-                    <h1><i class="fa-regular fa-circle-user"></i></h1>
+                    <h1 className="icon mb-4"><i className="fa-regular fa-circle-user"></i></h1>
                     
                     <div class="form-floating my-3">
                         <input type="email" class="form-control" id="floatingInput" placeholder="nombre@ejemplo.com"/>
-                        <label for="floatingInput">Email</label>
+                        <label className="my_label" for="floatingInput">Email</label>
                     </div>
                     <div class="form-floating my-3">
                         <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña"/>
-                        <label for="floatingPassword">Contraseña</label>
+                        <label className="my_label" for="floatingPassword">Contraseña</label>
                     </div>
                     
                     <div class="checkbox mb-3">
-                        <label>
+                        <label className="my_label mb-4">
                             <input type="checkbox" value="remember-me"/> Recordarme
                         </label>
                     </div>
-                    <button class="w-75 btn btn-lg btn-primary" type="submit">Iniciar Sesión</button>
+                    <button className="primario w-75 btn btn-lg" type="submit">Iniciar Sesión</button>
                 </form>
             </div>
 
             <div className= "col-lg-6 align-items-stretch">
                 <div className="row">
-                    <div class="h-100 p-5 text-white bg-dark rounded-3 mb-3 shadow">
+                    <div class="jumbotron_3 h-100 p-5 text-white rounded-3 mb-3 shadow">
                         <h3 className="fs-5 text">¿Aun no te has registrado?</h3>
                         <p>Necesitarás el código de registro de la ONG en la que trabajas.</p>
-                        <button class="btn btn-outline-light" type="button">Registrarse</button>
+                        <Link to="/user_registration"><button class="btn btn-outline-light" type="button">Registrarse</button></Link>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div class="h-100 p-5 bg-light border rounded-3 mb-3 shadow">
+                    <div class="jumbotron_2 h-100 p-5 bg-light border rounded-3 mb-3 shadow">
                         <h3 className="fs-5 text">Registro de ONG</h3>
                         <p>Antes de registrarte, debes asegurarte de registrar la ONG en la que trabajas.</p>
-                        <button class="btn btn-outline-secondary" type="button">Registrar ONG</button>
+                        <Link to="/ong_registration"><button className="terciario btn" type="button">Registrar ONG</button></Link>
                     </div>
                 </div>
             </div>
