f2c7f9af7f14d184a622143d9c53bf4a7cc276b5
julitar <126813769+julitar@users.noreply.github.com>
2023-07-17 13:58:29 +0000

    diseño cards y carousel

--- src/front/js/pages/detallerecurso.js
+++ src/front/js/pages/detallerecurso.js
@@ -33,7 +33,7 @@ export const Detallerecurso = () => {
     <>
       <div className="container-fluid">
         <div className="row text-center align-items-center justify-content-center m-auto">
-          <div className="col-10 jumbotron_4 p-3 mt-4 border rounded-3 mb-3">
+          <div className="col-lg-6 col-md-10 col-sm-11 jumbotron_4 p-3 mt-4 border rounded-3 mb-3">
             <h5 className="mi_titulo">Información del recurso <span className="texto_especial">{info.nombre}</span></h5>
           </div>
             
@@ -52,8 +52,10 @@ export const Detallerecurso = () => {
             />
 
 
-          <div className="row px-5 py-3 ml-3">
+          <div className="row">
+            <div className="col-lg-6 col-md-10 col-sm-11 px-5 py-3 ml-5">
             <button className="btn secundario" style={{ width: '150px' }} onClick={()=> navigate(-1)}>Volver</button>
+          </div>
           </div>
         </div>
       </div>
--- src/front/js/pages/ongCard.js
+++ src/front/js/pages/ongCard.js
@@ -19,19 +19,19 @@ export const OngCard = () => {
 
   return (
     <>
-      <div className="row  d-flex justify-content-center">
-        <div className="col-8">
-          <div className="row d-flex justify-content-center m-5 card shadow">
+      <div className="row justify-content-center m-3">
+        <div className="col-lg-6 col-md-11 col-sm-11">
+          <div className="row justify-content-center card shadow">
             <div className="card-header">
               <h4 className="titulo_1 text-center"><strong>{store.infoOng.nombre}</strong></h4>
             </div>
             <div className="row card-body">
-              <div className="col-lg-6 col-md-12">
+              <div className="col-lg-6 col-md-12 col-sm-12">
                 <div className="text-center m-3">
                   <img src={store.infoOng.logo} alt="Imagen" className="img-fluid" style={{ maxWidth : '250px'}}/>
                 </div> 
               </div>
-              <div className="col-lg-6 col-md-12">
+              <div className="col-lg-6 col-md-12 col-sm-12">
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item"><strong><i className="fas fa-at"></i> Email:</strong> {store.infoOng.email}</li>
                   <li className="list-group-item"><strong><i className="fas fa-map-marker-alt"></i> Dirección:</strong> {store.infoOng.direccion} - {store.infoOng.codigo_postal}</li>
@@ -44,9 +44,16 @@ export const OngCard = () => {
         </div>
       </div>
         
-      <div className= "row d-flex mx-5">
-          {store.recursosOng.map((recurso) => ( 
-            <div className="col-12 col-md-4 col-lg-3 justify-content-center m-auto" key={recurso.id}>
+      <div className="row m-3">
+        <div className="row text-center align-items-center justify-content-center m-auto">
+          <div className="col-lg-6 col-md-10 col-sm-11 jumbotron_4 p-3 border rounded-3">
+            <h5 className="mi_titulo">Estos son los recursos que ofrece <span className="texto_especial">{store.infoOng.nombre}</span></h5>
+          </div>
+        </div>
+        <div id="carouselExample" className="col-lg-6 col-sm-12 align-items-center justify-content-center m-auto carousel carousel-dark slide" data-bs-ride="carousel" data-bs-interval="false">
+          <div className="carousel-inner">
+          {store.recursosOng.map((recurso, index) => ( 
+            <div className= {`carousel-item my-3${index === 0 ? ' active' : ''}`} key={recurso.id}>
               <CardRecursos
                 key={recurso.id}
                 id={recurso.id}
@@ -59,6 +66,18 @@ export const OngCard = () => {
         ))}
         
       </div>
+
+      <button className="carousel-control-prev w-auto" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next w-auto" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+
+        </div>
+      </div>  
         
       <div className="text-start m-5 ">
         <button className="btn secundario" style={{ width: '150px' }} onClick={()=> navigate(-1)}>Volver</button>
